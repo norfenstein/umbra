norfenstein/umbra

commit 247854ab07bc1049be4017d99f5a8726cfbab0e2

* (bug 3291) Add /buildlog and /revert commands to combat grief building (Undeference, Rezyn, and originally Benmachine)
git-svn-id: svn://svn.icculus.org/tremulous/trunk@2020 fa00571f-6d07-0410-8660-cfe97aa2585a

src/game/g_utils.c
@@ -555,8 +555,8 @@ void G_KillBox( gentity_t *ent )
   gentity_t *hit;
   vec3_t    mins, maxs;
 
-  VectorAdd( ent->client->ps.origin, ent->r.mins, mins );
-  VectorAdd( ent->client->ps.origin, ent->r.maxs, maxs );
+  VectorAdd( ent->s.origin, ent->r.mins, mins );
+  VectorAdd( ent->s.origin, ent->r.maxs, maxs );
   num = trap_EntitiesInBox( mins, maxs, touch, MAX_GENTITIES );
 
   for( i = 0; i < num; i++ )

src/game/g_admin.c
@@ -75,6 +75,11 @@ g_admin_cmd_t g_admin_cmds[ ] =
       "[^3name|slot#|IP(/mask)^7] (^5duration^7) (^5reason^7)"
     },
 
+    {"buildlog", G_admin_buildlog, "buildlog",
+      "show buildable log",
+      "(^5name|slot#^7) (^5id^7)"
+    },
+
     {"cancelvote", G_admin_endvote, "cancelvote",
       "cancel a vote taking place",
       "(^5a|h^7)"
@@ -153,6 +158,11 @@ g_admin_cmd_t g_admin_cmds[ ] =
     {"restart", G_admin_restart, "restart",
       "restart the current map (optionally using named layout or keeping/switching teams)",
       "(^5layout^7) (^5keepteams|switchteams|keepteamslock|switchteamslock^7)"
+    },
+
+    {"revert", G_admin_revert, "revert",
+      "revert buildables to a given time",
+      "[^3id^7]"
     },
 
     {"setlevel", G_admin_setlevel, "setlevel",
@@ -783,6 +793,7 @@ static int admin_listadmins( gentity_t *ent, int start, char *search )
   return drawn;
 }
 
+#define MAX_DURATION_LENGTH 13
 void G_admin_duration( int secs, char *duration, int dursize )
 {
   // sizeof("12.5 minutes") == 13
@@ -826,7 +837,7 @@ qboolean G_admin_ban_check( gentity_t *ent, char *reason, int rlen )
       ( !G_admin_permission( ent, ADMF_IMMUNITY ) &&
         G_AddressCompare( &ban->ip, &ent->client->pers.ip ) ) )
     {
-      char duration[ 13 ];
+      char duration[ MAX_DURATION_LENGTH ];
       G_admin_duration( ban->expires - t,
         duration, sizeof( duration ) );
       if( reason )
@@ -1431,7 +1442,7 @@ qboolean G_admin_ban( gentity_t *ent )
   char search[ MAX_NAME_LENGTH ];
   char secs[ MAX_TOKEN_CHARS ];
   char *reason;
-  char duration[ 13 ];
+  char duration[ MAX_DURATION_LENGTH ];
   int logmatches = 0;
   int i;
   qboolean exactmatch = qfalse;
@@ -1674,7 +1685,7 @@ qboolean G_admin_adjustban( gentity_t *ent )
   int length, maximum;
   int expires;
   int time = trap_RealTime( NULL );
-  char duration[ 13 ] = {""};
+  char duration[ MAX_DURATION_LENGTH ] = {""};
   char *reason;
   char bs[ 5 ];
   char secs[ MAX_TOKEN_CHARS ];
@@ -2193,7 +2204,7 @@ qboolean G_admin_showbans( gentity_t *ent )
   int found = 0;
   int count;
   int t;
-  char duration[ 13 ];
+  char duration[ MAX_DURATION_LENGTH ];
   int max_name = 1, max_banner = 1;
   int colorlen1, colorlen2;
   int len;
@@ -2810,6 +2821,167 @@ qboolean G_admin_lock( gentity_t *ent )
     command, BG_TeamName( team ), lock ? "" : "un",
     ent ? ent->client->pers.netname : "console" ) );
 
+  return qtrue;
+}
+
+static char *fates[] =
+{
+  "^2built^7",
+  "^3deconstructed^7",
+  "^7replaced^7",
+  "^3destroyed^7",
+  "^7unpowered^7",
+  "removed"
+};
+qboolean G_admin_buildlog( gentity_t *ent )
+{
+  char       search[ MAX_NAME_LENGTH ] = {""};
+  char       s[ MAX_NAME_LENGTH ] = {""};
+  char       n[ MAX_NAME_LENGTH ];
+  char       stamp[ 8 ];
+  int        id = -1;
+  int        printed = 0;
+  int        time;
+  int        start = MAX_CLIENTS + level.buildId - level.numBuildLogs;
+  int        i = 0, j;
+  buildLog_t *log;
+
+  if( !level.buildId )
+  {
+    ADMP( "^3buildlog: ^7log is empty\n" );
+    return qtrue;
+  }
+
+  if( trap_Argc() == 3 )
+  {
+    trap_Argv( 2, search, sizeof( search ) );
+    start = atoi( search );
+  }
+  if( trap_Argc() > 1 )
+  {
+    trap_Argv( 1, search, sizeof( search ) );
+    for( i = search[ 0 ] == '-'; isdigit( search[ i ] ); i++ );
+    if( i && !search[ i ] )
+    {
+      id = atoi( search );
+      if( trap_Argc() == 2 && ( id < 0 || id >= MAX_CLIENTS ) )
+      {
+        start = id;
+        id = -1;
+      }
+      else if( id < 0 || id >= MAX_CLIENTS ||
+        level.clients[ id ].pers.connected != CON_CONNECTED )
+      {
+        ADMP( "^3buildlog: ^7invalid client id\n" );
+        return qfalse;
+      }
+    }
+    else
+      G_SanitiseString( search, s, sizeof( s ) );
+  }
+  else
+    start = MAX( -MAX_ADMIN_LISTITEMS, -level.buildId );
+
+  if( start < 0 )
+    start = MAX( level.buildId - level.numBuildLogs, start + level.buildId );
+  else
+    start -= MAX_CLIENTS;
+  if( start < level.buildId - level.numBuildLogs || start >= level.buildId )
+  {
+    ADMP( "^3buildlog: ^7invalid build ID\n" );
+    return qfalse;
+  }
+
+  trap_SendServerCommand( -1,
+    va( "print \"^3buildlog: ^7%s^7 requested a log of recent building activity\n\"",
+        ent ? ent->client->pers.netname : "console"  ) );
+
+  ADMBP_begin();
+  for( i = start; i < level.buildId && printed < MAX_ADMIN_LISTITEMS; i++ )
+  {
+    log = &level.buildLog[ i % MAX_BUILDLOG ];
+    if( id >= 0 && id < MAX_CLIENTS )
+    {
+      if( log->actor != level.clients[ id ].pers.namelog )
+        continue;
+    }
+    else if( s[ 0 ] )
+    {
+      if( !log->actor )
+        continue;
+      for( j = 0; j < MAX_NAMELOG_NAMES && log->actor->name[ j ][ 0 ]; j++ )
+      {
+        G_SanitiseString( log->actor->name[ j ], n, sizeof( n ) );
+        if( strstr( n, s ) )
+          break;
+      }
+      if( j >= MAX_NAMELOG_NAMES || !log->actor->name[ j ][ 0 ] )
+        continue;
+    }
+    printed++;
+    time = ( log->time - level.startTime ) / 1000;
+    Com_sprintf( stamp, sizeof( stamp ), "%3d:%02d", time / 60, time % 60 );
+    ADMBP( va( "^2%c^7%-3d %s ^7%s^7 %s%s%s\n",
+      log->actor && log->fate != BF_REPLACE && log->fate != BF_UNPOWER ?
+        '*' : ' ',
+      i + MAX_CLIENTS,
+      log->actor && ( log->fate == BF_REPLACE || log->fate == BF_UNPOWER ) ?
+        "    \\_" : stamp,
+      BG_Buildable( log->modelindex )->humanName,
+      fates[ log->fate ],
+      log->actor ? " by " : "",
+      log->actor ?
+        log->actor->name[ log->actor->nameChanges % MAX_NAMELOG_NAMES ] :
+        "" ) );
+  }
+  ADMBP( va( "^3buildlog: ^7showing %d build logs %d - %d of %d - %d.  %s\n",
+    printed, start + MAX_CLIENTS, i + MAX_CLIENTS - 1,
+    level.buildId + MAX_CLIENTS - level.numBuildLogs,
+    level.buildId + MAX_CLIENTS - 1,
+    i < level.buildId ? va( "run 'buildlog %s%s%d' to see more",
+      search, search[ 0 ] ? " " : "", i + MAX_CLIENTS ) : "" ) );
+  ADMBP_end();
+  return qtrue;
+}
+
+qboolean G_admin_revert( gentity_t *ent )
+{
+  char       arg[ MAX_TOKEN_CHARS ];
+  char       time[ MAX_DURATION_LENGTH ];
+  int        id;
+  buildLog_t *log;
+
+  if( trap_Argc() != 2 )
+  {
+    ADMP( "^3revert: ^7usage: revert [id]\n" );
+    return qfalse;
+  }
+
+  trap_Argv( 1, arg, sizeof( arg ) );
+  id = atoi( arg ) - MAX_CLIENTS;
+  if( id < level.buildId - level.numBuildLogs || id >= level.buildId )
+  {
+    ADMP( "^3revert: ^7invalid id\n" );
+    return qfalse;
+  }
+
+  log = &level.buildLog[ id % MAX_BUILDLOG ];
+  if( !log->actor || log->fate == BF_REPLACE || log->fate == BF_UNPOWER )
+  {
+    // fixme: then why list them with an id # in build log ? - rez
+    ADMP( "^3revert: ^7you can only revert direct player actions, "
+      "indicated by ^2* ^7in buildlog\n" );
+    return qfalse;
+  }
+
+  G_admin_duration( ( level.time - log->time ) / 1000, time,
+    sizeof( time ) );
+  AP( va( "print \"^3revert: ^7%s^7 reverted %d %s over the past %s\n\"",
+    ent ? ent->client->pers.netname : "console",
+    level.buildId - id,
+    ( level.buildId - id ) > 1 ? "changes" : "change",
+    time ) );
+  G_BuildLogRevert( id );
   return qtrue;
 }
 

src/game/g_admin.h
@@ -171,6 +171,8 @@ qboolean G_admin_restart( gentity_t *ent );
 qboolean G_admin_nextmap( gentity_t *ent );
 qboolean G_admin_namelog( gentity_t *ent );
 qboolean G_admin_lock( gentity_t *ent );
+qboolean G_admin_buildlog( gentity_t *ent );
+qboolean G_admin_revert( gentity_t *ent );
 
 void G_admin_print( gentity_t *ent, char *m );
 void G_admin_buffer_print( gentity_t *ent, char *m );

src/game/g_buildable.c
@@ -2474,15 +2474,15 @@ void HTeslaGen_Think( gentity_t *self )
 
 /*
 ============
-G_QueueBuildPoints
+G_QueueValue
 ============
 */
-void G_QueueBuildPoints( gentity_t *self )
+
+static int G_QueueValue( gentity_t *self )
 {
-  gentity_t *powerEntity;
   int       i;
   int       damageTotal = 0;
-  int       queuePoints = 0;
+  int       queuePoints;
   double    queueFraction = 0;
 
   for( i = 0; i < level.maxclients; i++ )
@@ -2501,6 +2501,20 @@ void G_QueueBuildPoints( gentity_t *self )
     queueFraction = 1.0;
 
   queuePoints = (int) ( queueFraction * (double) BG_Buildable( self->s.modelindex )->buildPoints );
+  return queuePoints;
+}
+
+/*
+============
+G_QueueBuildPoints
+============
+*/
+void G_QueueBuildPoints( gentity_t *self )
+{
+  gentity_t *powerEntity;
+  int       queuePoints;
+
+  queuePoints = G_QueueValue( self );
 
   if( !queuePoints )
     return;
@@ -3423,6 +3437,12 @@ static gentity_t *G_Build( gentity_t *builder, buildable_t buildable, vec3_t ori
   vec3_t    normal;
   char      readable[ MAX_STRING_CHARS ];
   char      buildnums[ MAX_STRING_CHARS ];
+  buildLog_t *log;
+
+  if( builder->client )
+    log = G_BuildLogNew( builder, BF_CONSTRUCT );
+  else
+    log = NULL;
 
   // Free existing buildables
   G_FreeMarkedBuildables( builder, readable, sizeof( readable ),
@@ -3654,6 +3674,9 @@ static gentity_t *G_Build( gentity_t *builder, buildable_t buildable, vec3_t ori
       readable );
   }
 
+  if( log )
+    G_BuildLogSet( log, built );
+
   return built;
 }
 
@@ -3742,7 +3765,7 @@ Traces down to find where an item should rest, instead of letting them
 free fall from their spawn points
 ================
 */
-static void G_FinishSpawningBuildable( gentity_t *ent )
+static gentity_t *G_FinishSpawningBuildable( gentity_t *ent, qboolean force )
 {
   trace_t     tr;
   vec3_t      dest;
@@ -3750,7 +3773,6 @@ static void G_FinishSpawningBuildable( gentity_t *ent )
   buildable_t buildable = ent->s.modelindex;
 
   built = G_Build( ent, buildable, ent->s.pos.trBase, ent->s.angles );
-  G_FreeEntity( ent );
 
   built->takedamage = qtrue;
   built->spawned = qtrue; //map entities are already spawned
@@ -3763,12 +3785,12 @@ static void G_FinishSpawningBuildable( gentity_t *ent )
 
   trap_Trace( &tr, built->s.origin, built->r.mins, built->r.maxs, dest, built->s.number, built->clipmask );
 
-  if( tr.startsolid )
+  if( tr.startsolid && !force )
   {
     G_Printf( S_COLOR_YELLOW "G_FinishSpawningBuildable: %s startsolid at %s\n",
               built->classname, vtos( built->s.origin ) );
     G_FreeEntity( built );
-    return;
+    return NULL;
   }
 
   //point items in the correct direction
@@ -3780,6 +3802,20 @@ static void G_FinishSpawningBuildable( gentity_t *ent )
   G_SetOrigin( built, tr.endpos );
 
   trap_LinkEntity( built );
+  return built;
+}
+
+/*
+============
+G_SpawnBuildableThink
+
+Complete spawning a buildable using it's placeholder
+============
+*/
+static void G_SpawnBuildableThink( gentity_t *ent )
+{
+  G_FinishSpawningBuildable( ent, qfalse );
+  G_FreeEntity( ent );
 }
 
 /*
@@ -3799,7 +3835,7 @@ void G_SpawnBuildable( gentity_t *ent, buildable_t buildable )
   // some movers spawn on the second frame, so delay item
   // spawns until the third frame so they can ride trains
   ent->nextthink = level.time + FRAMETIME * 2;
-  ent->think = G_FinishSpawningBuildable;
+  ent->think = G_SpawnBuildableThink;
 }
 
 /*
@@ -4100,6 +4136,177 @@ void G_BaseSelfDestruct( team_t team )
     if( ent->buildableTeam != team )
       continue;
     G_Damage( ent, NULL, NULL, NULL, NULL, 10000, 0, MOD_SUICIDE );
+  }
+}
+
+/*
+============
+build log
+============
+*/
+buildLog_t *G_BuildLogNew( gentity_t *actor, buildFate_t fate )
+{
+  buildLog_t *log = &level.buildLog[ level.buildId++ % MAX_BUILDLOG ];
+
+  if( level.numBuildLogs < MAX_BUILDLOG )
+    level.numBuildLogs++;
+  log->time = level.time;
+  log->fate = fate;
+  log->actor = actor && actor->client ? actor->client->pers.namelog : NULL;
+  return log;
+}
+
+void G_BuildLogSet( buildLog_t *log, gentity_t *ent )
+{
+  log->modelindex = ent->s.modelindex;
+  log->deconstruct = log->deconstruct;
+  log->deconstructTime = ent->deconstructTime;
+  VectorCopy( ent->s.pos.trBase, log->origin );
+  VectorCopy( ent->s.angles, log->angles );
+  VectorCopy( ent->s.origin2, log->origin2 );
+  VectorCopy( ent->s.angles2, log->angles2 );
+  log->powerSource = ent->parentNode ? ent->parentNode->s.modelindex : BA_NONE;
+  log->powerValue = G_QueueValue( ent );
+}
+
+void G_BuildLogAuto( gentity_t *actor, gentity_t *buildable, buildFate_t fate )
+{
+  G_BuildLogSet( G_BuildLogNew( actor, fate ), buildable );
+}
+
+void G_BuildLogRevertThink( gentity_t *ent )
+{
+  gentity_t *built;
+  vec3_t    mins, maxs;
+  int       blockers[ MAX_GENTITIES ];
+  int       num;
+  int       victims = 0;
+  int       i;
+
+  if( ent->suicideTime > level.time )
+  {
+    BG_BuildableBoundingBox( ent->s.modelindex, mins, maxs );
+    VectorAdd( ent->s.pos.trBase, mins, mins );
+    VectorAdd( ent->s.pos.trBase, maxs, maxs );
+    num = trap_EntitiesInBox( mins, maxs, blockers, MAX_GENTITIES );
+    for( i = 0; i < num; i++ )
+    {
+      gentity_t *targ;
+      vec3_t    push;
+
+      targ = g_entities + blockers[ i ];
+      if( targ->client )
+      {
+        float val = ( targ->client->ps.eFlags & EF_WALLCLIMB) ? 300.0 : 150.0;
+
+        VectorSet( push, crandom() * val, crandom() * val, random() * val );
+        VectorAdd( targ->client->ps.velocity, push, targ->client->ps.velocity );
+        victims++;
+      }
+    }
+
+    if( victims )
+    {
+      // still a blocker
+      ent->nextthink = level.time + FRAMETIME;
+      return;
+    }
+  }
+
+  built = G_FinishSpawningBuildable( ent, qtrue );
+  if( ( built->deconstruct = ent->deconstruct ) )
+    built->deconstructTime = ent->deconstructTime;
+  built->buildTime = built->s.time = 0;
+  G_KillBox( built );
+
+  G_LogPrintf( "revert: restore %d %s\n",
+    built - g_entities, BG_Buildable( built->s.modelindex )->name );
+
+  G_FreeEntity( ent );
+}
+
+void G_BuildLogRevert( int id )
+{
+  buildLog_t *log;
+  gentity_t  *ent;
+  int        i;
+  vec3_t     dist;
+
+  level.numBuildablesForRemoval = 0;
+
+  level.numBuildLogs -= level.buildId - id;
+  while( level.buildId > id )
+  {
+    log = &level.buildLog[ --level.buildId % MAX_BUILDLOG ];
+    if( log->fate == BF_CONSTRUCT )
+    {
+      for( i = MAX_CLIENTS; i < level.num_entities; i++ )
+      {
+        ent = &g_entities[ i ];
+        if( ent->s.eType != ET_BUILDABLE ||
+          ent->s.modelindex != log->modelindex ||
+          ent->health <= 0 )
+          continue;
+
+        VectorSubtract( ent->s.pos.trBase, log->origin, dist );
+        if( VectorLengthSquared( dist ) > 2.0f )
+          continue;
+
+        G_LogPrintf( "revert: remove %d %s\n",
+          ent - g_entities, BG_Buildable( ent->s.modelindex )->name );
+        G_FreeEntity( ent );
+        break;
+      }
+    }
+    else
+    {
+      gentity_t  *builder = G_Spawn();
+
+      builder->client = NULL;
+      VectorCopy( log->origin, builder->s.pos.trBase );
+      VectorCopy( log->angles, builder->s.angles );
+      VectorCopy( log->origin2, builder->s.origin2 );
+      VectorCopy( log->angles2, builder->s.angles2 );
+      builder->s.modelindex = log->modelindex;
+      builder->deconstruct = log->deconstruct;
+      builder->deconstructTime = log->deconstructTime;
+
+      builder->think = G_BuildLogRevertThink;
+      builder->nextthink = level.time + FRAMETIME;
+      builder->suicideTime = level.time + 3000;
+
+      if( log->fate == BF_DESTROY )
+      {
+        int value = log->powerValue;
+
+        if( BG_Buildable( log->modelindex )->team == TEAM_ALIENS )
+        {
+          level.alienBuildPointQueue =
+            MAX( 0, level.alienBuildPointQueue - value );
+        }
+        else
+        {
+          if( log->powerSource == BA_H_REACTOR )
+          {
+            level.humanBuildPointQueue =
+              MAX( 0, level.humanBuildPointQueue - value );
+          }
+          else if( log->powerSource == BA_H_REPEATER )
+          {
+            gentity_t        *source;
+            buildPointZone_t *zone;
+
+            source = G_PowerEntityForPoint( log->origin );
+            if( source && source->usesBuildPointZone )
+            {
+              zone = &level.buildPointZones[ source->buildPointZone ];
+              zone->queuedBuildPoints =
+                MAX( 0, zone->queuedBuildPoints - value );
+            }
+          }
+        }
+      }
+    }
   }
 }
 

src/game/g_combat.c
@@ -1371,8 +1371,24 @@ Log deconstruct/destroy events
 */
 void G_LogDestruction( gentity_t *self, gentity_t *actor, int mod )
 {
-  if( !actor )
-    return;
+  buildFate_t fate;
+
+  switch( mod )
+  {
+    case MOD_DECONSTRUCT:
+      fate = BF_DECONSTRUCT;
+      break;
+    case MOD_REPLACE:
+      fate = BF_REPLACE;
+      break;
+    case MOD_NOCREEP:
+      fate = ( actor->client ) ? BF_UNPOWER : BF_AUTO;
+      break;
+    default:
+      fate = ( actor->client ) ? BF_DESTROY : BF_AUTO;
+      break;
+  }
+  G_BuildLogAuto( actor, self, fate );
 
   // don't log when marked structures are removed
   if( mod == MOD_REPLACE )

src/game/g_local.h
@@ -491,11 +491,40 @@ typedef enum
   TW_PASSED
 } timeWarning_t;
 
+// fate of a buildable
+typedef enum
+{
+  BF_CONSTRUCT,
+  BF_DECONSTRUCT,
+  BF_REPLACE,
+  BF_DESTROY,
+  BF_UNPOWER,
+  BF_AUTO
+} buildFate_t;
+
+// data needed to revert a change in layout
+typedef struct
+{
+  int          time;
+  buildFate_t  fate;
+  namelog_t    *actor;
+  buildable_t  modelindex;
+  qboolean     deconstruct;
+  int          deconstructTime;
+  vec3_t       origin;
+  vec3_t       angles;
+  vec3_t       origin2;
+  vec3_t       angles2;
+  buildable_t  powerSource;
+  int          powerValue;
+} buildLog_t;
+
 //
 // this structure is cleared as each map is entered
 //
 #define MAX_SPAWN_VARS      64
 #define MAX_SPAWN_VARS_CHARS  4096
+#define MAX_BUILDLOG          128
 
 typedef struct
 {
@@ -633,6 +662,10 @@ typedef struct
   int               emoticonCount;
 
   namelog_t         *namelogs;
+
+  buildLog_t        buildLog[ MAX_BUILDLOG ];
+  int               buildId;
+  int               numBuildLogs;
 } level_locals_t;
 
 #define CMD_CHEAT         0x0001
@@ -759,6 +792,10 @@ gentity_t         *G_PowerEntityForPoint( const vec3_t origin );
 gentity_t         *G_PowerEntityForEntity( gentity_t *ent );
 gentity_t         *G_RepeaterEntityForPoint( vec3_t origin );
 qboolean          G_InPowerZone( gentity_t *self );
+buildLog_t        *G_BuildLogNew( gentity_t *actor, buildFate_t fate );
+void              G_BuildLogSet( buildLog_t *log, gentity_t *ent );
+void              G_BuildLogAuto( gentity_t *actor, gentity_t *buildable, buildFate_t fate );
+void              G_BuildLogRevert( int id );
 
 //
 // g_utils.c

commit 3db6eff06be408a271f79314d83b60c4bc131b30

* Fix teleporters so that they once again telefrag players (broken by r2020)
git-svn-id: svn://svn.icculus.org/tremulous/trunk@2046 fa00571f-6d07-0410-8660-cfe97aa2585a

src/game/g_misc.c
@@ -91,10 +91,6 @@ void TeleportPlayer( gentity_t *player, vec3_t origin, vec3_t angles )
   // set angles
   G_SetClientViewAngle( player, angles );
 
-  // kill anything at the destination
-  if( player->client->sess.spectatorState == SPECTATOR_NOT )
-    G_KillBox( player );
-
   // save results of pmove
   BG_PlayerStateToEntityState( &player->client->ps, &player->s, qtrue );
 
@@ -102,7 +98,12 @@ void TeleportPlayer( gentity_t *player, vec3_t origin, vec3_t angles )
   VectorCopy( player->client->ps.origin, player->r.currentOrigin );
 
   if( player->client->sess.spectatorState == SPECTATOR_NOT )
+  {
+    // kill anything at the destination
+    G_KillBox( player );
+
     trap_LinkEntity (player);
+  }
 }
 
 

src/game/g_utils.c
@@ -555,8 +555,8 @@ void G_KillBox( gentity_t *ent )
   gentity_t *hit;
   vec3_t    mins, maxs;
 
-  VectorAdd( ent->s.origin, ent->r.mins, mins );
-  VectorAdd( ent->s.origin, ent->r.maxs, maxs );
+  VectorAdd( ent->r.currentOrigin, ent->r.mins, mins );
+  VectorAdd( ent->r.currentOrigin, ent->r.maxs, maxs );
   num = trap_EntitiesInBox( mins, maxs, touch, MAX_GENTITIES );
 
   for( i = 0; i < num; i++ )